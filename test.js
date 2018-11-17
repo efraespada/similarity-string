1f7d6ed1b8159e6bd9e66586a3a683dda8a6e263
Efra Espada <efraespada@gmail.com>
2018-11-17 15:17:21 +0100

    travis

--- test.js
+++ test.js
@@ -1,29 +1,35 @@
 String.similarity = require("./index");
 const sim = require('similarity');
+const logjs = require('logjsx');
+const leven = require('leven');
 const list = require("./values").LIST;
-describe('similarity test', function () {
+let logger = new logjs();
+logger.init({
+    level : "DEBUG"
+});
+describe('similarity-string vs leven test', function () {
     for (let c in list) {
-        it(`similarity ${list[c][0]} and ${list[c][1]}`, function (done) {
+        it(`${list[c][0]} and ${list[c][1]}`, function (done) {
             let similarity = String.similarity(list[c][0], list[c][1]);
             let bar = "[";
             for (let _i = 0; _i < Math.round(similarity); _i++) bar += "░"
             for (let _i = 0; _i < 100 - Math.round(similarity); _i++) bar += " "
             bar += "]";
-            console.log(`${list[c][0]} - ${list[c][1]}: ${similarity.toFixed(2)}% ${bar}`);
+            logger.info(`${list[c][0]} - ${list[c][1]}: ${similarity.toFixed(2)}% ${bar} leven: ${leven(list[c][0], list[c][1])}`);
             done();
         });
     }
 });
 
-describe('levenshtein similarity test', function () {
+describe('levenshtein comparision test (similarity + leven)', function () {
     for (let c in list) {
-        it(`levenshtein similarity ${list[c][0]} and ${list[c][1]}`, function (done) {
+        it(`${list[c][0]} and ${list[c][1]}`, function (done) {
             let similarity = sim(list[c][0], list[c][1]) * 100;
             let bar = "[";
             for (let _i = 0; _i < Math.round(similarity); _i++) bar += "░"
             for (let _i = 0; _i < 100 - Math.round(similarity); _i++) bar += " "
             bar += "]";
-            console.log(`${list[c][0]} - ${list[c][1]}: ${similarity.toFixed(2)}% ${bar}`);
+            logger.warn(`${list[c][0]} - ${list[c][1]}: ${similarity.toFixed(2)}% (leven: ${leven(list[c][0], list[c][1])}) ${bar}`);
             done();
         });
     }
